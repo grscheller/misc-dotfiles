9daaada098ed6c18e5e27c420ffc87d0e1b2cb2a
grscheller <geoffrey@scheller.com>
2026-08-25 02:41:20 -0600

    Updated bin/sourced/configure_xdg_dirs.sh
    For Windows/Linux compatibility.

--- bin/sourced/configure_xdg_dirs.sh
+++ bin/sourced/configure_xdg_dirs.sh
@@ -1,51 +1,44 @@
 ## Setup XDG Desktop folder locations
 #
-# grscheller/misc-dotfiles uses these names in its setup scripts.
+# grscheller/misc-dotfiles uses XDG names in its setup scripts.
 #
 # - defaults to standard locations if not already defined
-# - tries to ensures directories exist
-# - failure to create will be indicated via stderr
+#   - can override in shell for multiple configs to exist
+#   - LINUX & MSYS2 tooling uses these when exported
+# - tries to ensure XDG directories exist
+# - XDG names can be overridden for multiple configurations to coexist
 #
 # shellcheck shell=sh
 
 # Script use XDG Desktop names, even on Windows 11
 
-if test "$XDG_CONFIG_HOME" = ""
-then
-    _ensure_xdg_dirs_exist=no
-else
-    _ensure_xdg_dirs_exist=yes
-fi
-
 if test "$OS" = Windows_NT
 then
     # Window 11
     _local_app_data="$(cygpath -u "$LOCALAPPDATA")"
-    : "${XDG_CONFIG_HOME:=$_local_app_data}"
-    : "${XDG_DATA_HOME:=$_local_app_data}"
-    : "${XDG_STATE_HOME:=$_local_app_data}"
-    : "${XDG_CACHE_HOME:=$_local_app_data/Temp}"
-else
-    # Linux
-    _ensure_xdg_dirs_exist=yes
+    : "${WIN_LOCAL_APP_DATA:=$_local_app_data}"
+    : "${WIN_CACHE_HOME:=$_local_app_data/Temp}"
+    unset _local_app_data
     : "${XDG_CONFIG_HOME:=$HOME/.config}"
     : "${XDG_DATA_HOME:=$HOME/.local/share}"
     : "${XDG_STATE_HOME:=$HOME/.local/state}"
     : "${XDG_CACHE_HOME:=$HOME/.cache}"
-fi
-export XDG_CONFIG_HOME XDG_DATA_HOME XDG_STATE_HOME XDG_CACHE_HOME
-
-if test "$_ensure_xdg_dirs_exist" = "yes"
-then
-    ensure_dir "$XDG_CONFIG_HOME" >&2
-    chmod 0755 "$XDG_CONFIG_HOME"
-    ensure_dir "$XDG_DATA_HOME" >&2
-    chmod 0755 "$XDG_DATA_HOME"
-    ensure_dir "$XDG_STATE_HOME" >&2
-    chmod 0755 "$XDG_STATE_HOME"
-    ensure_dir "$XDG_CACHE_HOME" >&2
-    chmod 0755 "$XDG_CACHE_HOME"
+else
+    # Linux
+    : "${XDG_CONFIG_HOME:=$HOME/.config}"
+    : "${XDG_DATA_HOME:=$HOME/.local/share}"
+    : "${XDG_STATE_HOME:=$HOME/.local/state}"
+    : "${XDG_CACHE_HOME:=$HOME/.cache}"
+    : "${WIN_LOCAL_APP_DATA:=}"
+    : "${WIN_CACHE_HOME:=}"
+    export XDG_CONFIG_HOME XDG_DATA_HOME XDG_STATE_HOME XDG_CACHE_HOME
 fi
 
-unset _ensure_xdg_dirs_exist
-unset _local_app_data
+ensure_dir "$XDG_CONFIG_HOME" >&2
+chmod 0755 "$XDG_CONFIG_HOME"
+ensure_dir "$XDG_DATA_HOME" >&2
+chmod 0755 "$XDG_DATA_HOME"
+ensure_dir "$XDG_STATE_HOME" >&2
+chmod 0755 "$XDG_STATE_HOME"
+ensure_dir "$XDG_CACHE_HOME" >&2
+chmod 0755 "$XDG_CACHE_HOME"
